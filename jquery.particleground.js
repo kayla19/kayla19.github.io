ddc6b201cbf657cfaca81dd4083bbf4e401cc2ae
Kayla Smith <smith.nak@husky.neu.edu>
2015-01-03 22:31:01 -0500

    Removed jQuery ref, insert particleground ref

--- jquery.particleground.js
+++ jquery.particleground.js
@@ -1,14 +1,38 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Nakayla Smith</title>
-	</head>
-	<body>
-		<script src="jquery.particleground"></script>
-		<script>
-		
-		;(function($) {
+/**
+ * Particleground
+ *
+ * @author Jonathan Nicol - @mrjnicol
+ * @version 1.0.1
+ * @description Creates a canvas based particle system background
+ *
+ * Inspired by:
+ * http://requestlab.fr/
+ * http://disruptivebydesign.com/
+ * 
+ * @license The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 Jonathan Nicol - @mrjnicol
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+;(function($) {
   var pluginName = 'particleground';
 
   function Plugin(element, options) {
@@ -412,10 +436,4 @@
         window.cancelAnimationFrame = function(id) {
             clearTimeout(id);
         };
-
-		
-		</script>
-		<h1>Hello World</h1>
-		<p>I'm hosted on Github!</p>
-	</body>
-</html>
+}());
