c4794eb734147130b30c78ac8fc76b1f5b6349e3
Kayla Smith <kayla19@users.noreply.github.com>
2015-10-31 19:11:49 -0400

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $('#particles').particleground({
     dotColor: '##7D5D4E',
     lineColor: '#7D5D4E',
-	density: '6000'
+	density: '2000'
   });
   $('.intro').css({
     'margin-top': -($('.intro').height() / 2)
