549701c4fdfe7bfa7169cc373d7f63232a17bf30
Kayla Smith <kayla19@users.noreply.github.com>
2015-10-31 19:20:16 -0400

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     dotColor: '#7D5D4E',
     lineColor: '#7D5D4E',
     particleRadius: 7, // Dot size
-    lineWidth: .5,
+    lineWidth: .25,
     curvedLines: false,
     proximity: 100,
 	density: '10000'
